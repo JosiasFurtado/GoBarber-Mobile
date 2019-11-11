624f23c7bc7c5112e334043cfb6b8e8cba25e858
Josias Furtado <53015921+JosiasFurtado@users.noreply.github.com>
2019-11-11 20:06:09 -0300

    Add files via upload

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment({
+  data: { provider, date, past, cancelable, canceled_at },
+  onCancel,
+}) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [date]);
+
+  return (
+    <Container past={past}>
+      <Left>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url.replace('localhost', '10.0.2.2')
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Info>
+          <Name>{provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
+
+      {cancelable && !canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
+    </Container>
+  );
+}
